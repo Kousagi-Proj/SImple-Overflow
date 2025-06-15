534d8bbd7120eb1b2a09091b7b444d343ad6475d
Kousagi-Proj <gravej347@gmail.com>
2025-06-15 03:58:38 -0400

    Update overload.c
    added recursive function 
    
    found that making x = 2 causes segmentation fault, set x = 9
    
    now constantly outputs current pointer location and value

--- overload.c
+++ overload.c
@@ -2,19 +2,25 @@
 #include <stdlib.h>
 #include <math.h>
 
-/*#include <stdio.h>
-int main () {
-	int x;
-	printf("%ld\n", sizeof(x)); //gives 4 as printed text
-	return 0;
+int recursion (int x){
+	int y = x;//set y to x
+	x = y * x;//multiply current x value by last x value (y = last x value)
+	if (x < 2000000){
+		return recursion (x);
+	}
+	return x;
 }
-*/
 int main(){
-	int x = 9*9*9*9*9*9*9*9*9*9*9*9*9*9; //creates a double which requires 8 bytes instead of 4 for int
+	int x = 9;
+	//creates a double which requires 8 bytes instead of 4 for int
 	//compiler will give warning of overflow, might cause crash
 	int * pointer = (int *) malloc(sizeof(int));
 	//printf("%ld", sizeof(*pointer)); //returns size of pointer which is 4 bytes
-	*pointer = x;
-	//TO-DO: multithread fuction to repeat until crash
+	while (recursion (x) < 9999999999) {
+		*pointer = recursion(x);
+		printf("x is located in: %p\n", (void*)pointer);//prints content of *pointer
+		printf("x = %d\n", recursion(x));//prints contnent of x, x to match *pointer
+	}
 	return 0;
 }
+
